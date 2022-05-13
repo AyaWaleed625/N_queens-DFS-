cf9034f8c86862375f26a434c328e42390723387
AyaWaleed625 <101192015+AyaWaleed625@users.noreply.github.com>
2022-05-13 11:46:32 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace ASQueen
+{
+    partial class NQueen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSolve = new System.Windows.Forms.Button();
+            this.pnlChess = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtN = new System.Windows.Forms.TextBox();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSolve
+            // 
+            this.btnSolve.Location = new System.Drawing.Point(208, 36);
+            this.btnSolve.Name = "btnSolve";
+            this.btnSolve.Size = new System.Drawing.Size(216, 33);
+            this.btnSolve.TabIndex = 0;
+            this.btnSolve.Text = "Start Solving";
+            this.btnSolve.UseVisualStyleBackColor = true;
+            this.btnSolve.Click += new System.EventHandler(this.btnSolve_Click);
+            // 
+            // pnlChess
+            // 
+            this.pnlChess.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlChess.Location = new System.Drawing.Point(29, 73);
+            this.pnlChess.Name = "pnlChess";
+            this.pnlChess.Size = new System.Drawing.Size(550, 550);
+            this.pnlChess.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(205, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Queen Count:";
+            // 
+            // txtN
+            // 
+            this.txtN.Location = new System.Drawing.Point(276, 9);
+            this.txtN.Name = "txtN";
+            this.txtN.Size = new System.Drawing.Size(148, 20);
+            this.txtN.TabIndex = 3;
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.lblTime.Location = new System.Drawing.Point(33, 47);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(43, 19);
+            this.lblTime.TabIndex = 4;
+            this.lblTime.Text = "Time";
+            // 
+            // NQueen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(604, 637);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.txtN);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pnlChess);
+            this.Controls.Add(this.btnSolve);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "NQueen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "N Queen Pronlem";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSolve;
+        private System.Windows.Forms.Panel pnlChess;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtN;
+        private System.Windows.Forms.Label lblTime;
+    }
+}
+
